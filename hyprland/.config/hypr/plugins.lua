0df4998105674b8bd16e3bc3b5a93e6d0bf0996c
dxlefourtwenty <ddparmstrong2021@gmail.com>
2026-05-21 00:28:12 -0700

    fixed special workspace focus

--- hyprland/.config/hypr/plugins.lua
+++ hyprland/.config/hypr/plugins.lua
@@ -1,4 +1,4 @@
-local enable_hyprspace = false
+local enable_hyprspace = true
 
 local dynamic_cursors_plugin = "/var/cache/hyprpm/dxle/dynamic-cursors/dynamic-cursors.so"
 local hyprspace_plugin = "/home/dxle/.config/hypr/.plugins/Hyprspace/Hyprspace.so"
@@ -41,55 +41,8 @@ hl.config({
 })
 ]]
 
-local hyprspace_keywords = {
-  "plugin:overview:panelColor rgba(00000000)",
-  "plugin:overview:panelBorderColor rgba(00000000)",
-  "plugin:overview:workspaceActiveBackground rgba(00000040)",
-  "plugin:overview:workspaceInactiveBackground rgba(00000080)",
-  "plugin:overview:workspaceActiveBorder rgba(ffffff40)",
-  "plugin:overview:workspaceInactiveBorder rgba(ffffff00)",
-  "plugin:overview:panelHeight 250",
-  "plugin:overview:panelBorderWidth 2",
-  "plugin:overview:workspaceMargin 12",
-  "plugin:overview:workspaceBorderSize 1",
-  "plugin:overview:centerAligned 1",
-  "plugin:overview:drawActiveWorkspace 1",
-  "plugin:overview:overrideGaps 1",
-  "plugin:overview:gapsIn 20",
-  "plugin:overview:gapsOut 60",
-  "plugin:overview:autoDrag 1",
-  "plugin:overview:autoScroll 1",
-  "plugin:overview:exitOnClick 1",
-  "plugin:overview:exitOnSwitch 0",
-  "plugin:overview:showNewWorkspace 1",
-  "plugin:overview:showEmptyWorkspace 1",
-  "plugin:overview:showSpecialWorkspace 0",
-  "plugin:overview:disableGestures 0",
-  "plugin:overview:disableBlur 1",
-  "plugin:overview:renderWindows 0",
-  "plugin:overview:renderLayers 0",
-  "plugin:overview:dragAlpha 0.2",
-  "plugin:overview:exitKey Escape",
-}
-
-local function keyword_command(keyword)
-  return "hyprctl keyword " .. keyword
-end
-
 local function shell_quote(value)
   return "'" .. value:gsub("'", "'\\''") .. "'"
-end
-
-local function active_plugin_keywords()
-  local keywords = {}
-
-  if enable_hyprspace then
-    for _, keyword in ipairs(hyprspace_keywords) do
-      table.insert(keywords, keyword)
-    end
-  end
-
-  return keywords
 end
 
 local function dynamic_cursors_config_command()
@@ -109,23 +62,15 @@ local function plugin_load_commands()
   return commands
 end
 
-local function apply_plugin_keywords()
+local function apply_plugins()
   hl.exec_cmd(table.concat(plugin_load_commands(), " ; "))
-
-  for _, keyword in ipairs(active_plugin_keywords()) do
-    hl.exec_cmd(keyword_command(keyword))
-  end
 end
 
 hl.on("hyprland.start", function()
   local commands = plugin_load_commands()
 
-  for _, keyword in ipairs(active_plugin_keywords()) do
-    table.insert(commands, keyword_command(keyword))
-  end
-
   table.insert(commands, "hyprctl dismissnotify")
   hl.exec_cmd(table.concat(commands, " ; "))
 end)
 
-hl.on("config.reloaded", apply_plugin_keywords)
+hl.on("config.reloaded", apply_plugins)
