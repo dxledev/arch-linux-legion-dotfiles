c559bffc07d024a62c2860e57b230f6d5ddbecb4
dxlefourtwenty <ddparmstrong2021@gmail.com>
2026-07-08 21:56:14 -0700

    added header file snippet

--- nvim/.config/nvim/lua/config/snippets_cpp.lua
+++ nvim/.config/nvim/lua/config/snippets_cpp.lua
@@ -2,6 +2,7 @@ local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
+local f = ls.function_node
 
 ls.add_snippets("cpp", {
   s("stdo", {
@@ -60,3 +61,38 @@ ls.add_snippets("cpp", {
   }),
 })
 
+ls.config.set_config({
+  enable_autosnippets = true,
+})
+
+local function guard_name(_, snip)
+  return snip.captures[1]
+end
+
+local hdef = s(
+  {
+    trig = "([%w_]+)%s+hdef",
+    regTrig = true,
+    wordTrig = false,
+    snippetType = "autosnippet",
+    name = "header guard",
+  },
+  {
+    t("#ifndef "),
+    f(guard_name),
+
+    t({ "", "#define " }),
+    f(guard_name),
+
+    t({ "", "" }),
+    i(1),
+
+    t({ "", "#endif" }),
+  }
+)
+
+for _, ft in ipairs({ "c", "cpp" }) do
+  ls.add_snippets(ft, {
+    hdef,
+  })
+end
